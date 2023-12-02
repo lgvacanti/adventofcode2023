3123c37641f897313daa405a69d053082c7f9e40
Liam Vacanti <11602015+lgvacanti@users.noreply.github.com>
2023-12-02 16:26:10 +0100

    initial setup

---
+++ day01/src/bin/part1.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_input() {
+        let input = include_str!("./example1.txt");
+        let result = part1(input);
+        assert_eq!(result, 142);
+    }
+}
